9d61328d8d562a55312051bc28890955c6cb6cc7
Nasadyuk Olga <nasaduk2005@mail.ru>
2023-12-11 00:15:31 +0500

    Update home_work.py

--- model/pydantic/home_work.py
+++ model/pydantic/home_work.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 class HomeTask(BaseModel):
     number: int
-    is_done: bool
+    is_done: bool = False
     last_try_time: datetime | None = None
     amount_tries: int = 0
 
