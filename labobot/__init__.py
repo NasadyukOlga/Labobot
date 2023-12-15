4a6207b9d4d4f87a5d731da752c65d49d96780e9
Nasadyuk Olga <nasaduk2005@mail.ru>
2023-12-15 12:13:22 +0500

    Create __init__.py

---
+++ labobot/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['bot']
+
+from labobot.configuration import bot
+import labobot.admin_handlers as admin_handlers
+import labobot.student_handlers as student_handlers
+import labobot.teacher_handlers as teacher_handlers
+import labobot.auth_handlers as auth_handlers
