29468e8c7b5be0d05ffc735592584e6816b519e4
Nasadyuk Olga <nasaduk2005@mail.ru>
2023-12-15 12:13:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import asyncio
+
 from utils.init_app import init_app
+from labobot import bot
+
+
+async def main():
+    await asyncio.gather(
+        bot.infinity_polling(request_timeout=90)
+    )
+
 
 if __name__ == '__main__':
     init_app()
+    asyncio.run(main())
