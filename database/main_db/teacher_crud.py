ca74d07c29fa32e1bffbe23dd6320f0feff3889e
Nasadyuk Olga <nasaduk2005@mail.ru>
2023-12-15 12:13:20 +0500

    Create teacher_crud.py

---
+++ database/main_db/teacher_crud.py
@@ -0,0 +1,11 @@
+from database.main_db.database import Session
+
+from model.main_db.teacher import Teacher
+
+
+def is_teacher(telegram_id: int) -> bool:
+    with Session() as session:
+        teacher = session.query(Teacher).filter(
+            Teacher.telegram_id == telegram_id
+        ).first()
+        return teacher is not None
